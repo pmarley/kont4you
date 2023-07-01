b400a5ce6bc326d7e58d7dd95dc97dbfe25d963f
Leonardo Amengual <leonardo@santins.com.br>
2023-07-01 01:25:40 -0300

    inicio da tela de cidadania portuguesa

---
+++ wp-content/themes/rgs-consultoria/tpl-cidadania-portuguesa.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Template Name: Cidadania portuguesa
+*/
+get_header();
+?>
+
+<section class="container-cidadania-portuguesa">
+    <div class="banner">
+        <img src="<?php echo image("BannerbandeiraPortugal.png") ?>" alt="">
+        <img src="<?php echo image("Rectanglebege.png") ?>" alt="">
+    </div>
+
+    <div class="session container">
+        <div class="left">
+            <h2>Quem tem direito a cidadania portuguesa?</h2>
+            <p>
+                A Cidadania Portuguesa é um direito transmitido por sangue (iure sanguinis), porém, existem outras formas de reconhecimento, assim como a naturalização portuguesa. Em todos os casos, nosso escritório trabalha em parceria com um renomado escritório de São Paulo e juntos organizamos e patrocinamos todas as fases deste processo, de forma pessoal e individualizada. No processo de solicitação da Cidadania Portuguesa não é necessário viajar a Portugal em nenhuma etapa. O Requerente será representado por nossa advogada junto à Conservatória, evitando assim incorrermos nos problemas burocráticos que são continuamente apresentados por Portugal para retardar ou impedir a conclusão exitosa dos processos, sempre que a parte tenta fazer diretamente pelos correios.
+            </p>
+            <h2>Beneficiados</h2>
+            <ul>
+                <li>Filhos de Português (mãe ou pai).</li>
+                <li>Netos de Português (avô ou avó).</li>
+                <li>Cônjuge (casamento civil) de Cidadão Português.</li>
+                <li>Menores de idade ou incapazes, filhos de pai ou mãe portugueses.</li>
+                <li>Residentes legais há 5 anos (no mínimo) em Portugal.</li>
+                <li>Indivíduos nascidos em Portugal, mesmo que filhos de pais estrangeiros, e que tenham morado no país legalmente por no mínimo 2 anos.</li>
+            </ul>
+        </div>
+        <div class="vertical-line"></div>
+        <div class="right">
+            <p><span class="text-importante">Importante:</span> Netos somente são reconhecidos como cidadãos portugueses quando há o falecimento do seu genitor. Se o genitor for vivo, este precisará fazer o processo primeiro, para depois o neto também ter direito. Caso o pai/mãe já for considerado português, o processo segue o rito tradicional.
+                Se o requerente vier a falecer antes do término do processo, o mesmo será cancelado, interrompendo assim, em alguns casos, a linha sucessória da cidadania.
+                Para avaliarmos o direito pretendido e sabermos o que ainda precisa ser feito para concluir o processo, bem como os custos até o reconhecimento do direito pretendido, faremos uma análise de toda a documentação reunida pelo Requerente.
+
+                Para uma analise eficaz o requerente deve reunir a seguinte documentação (reúna o máximo dos documentos abaixo que conseguir):
+                Certidão de Nascimento ou Batismo e Casamento (se houver) do Cidadão Português atualizada.
+                Certidões de Casamento (se houver) e Óbito do antepassado Português, em formato Cópia Reprográfica
+                Certidão de Nascimento do Requerente, em formato Cópia Reprográfica • Preencha a árvore genealógica anexa com os nomes dos ascendentes do Requerente até o (a) português (a).
+            </p>
+        </div>
+    </div>
+
+
+
+
+</section>
+
+<?php
+get_footer();
+?>
