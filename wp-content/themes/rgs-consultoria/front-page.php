4ebdbde0d3c7bc32bad1bd612ded73603b9460e3
Matheus Melo <matheus@santins.com.br>
2023-07-09 16:47:33 -0300

    wip: about us

--- wp-content/themes/rgs-consultoria/front-page.php
+++ wp-content/themes/rgs-consultoria/front-page.php
@@ -31,6 +31,43 @@
         Nossos Serviços
       </a>
     </div>
+
+    <div class="main-container-expirence">
+      <div class="container-expirence">
+        <div class="title-expirence">
+          <img src="<?= image('diploma.svg') ?>">
+          <p>Rinara Granato Santos<br />OAB/MG 96911</p>
+        </div>
+        <p>Advogada Internacionalista Professora de Direito Internacional e Direitos Humanos. Mestra em Educação Profissional e Tecnológica. Doutora em Ciências Jurídicas e Sociais.</p>
+      </div>
+      <div class="main-container-glass-more-info">
+        <h3>Para mais informações, entre em contato conosco por email ou nosso Whatsapp.</h3>
+      </div>
+    </div>
+    <div class="main-container-photo">
+      <img src="<?= image('person_1.png') ?>">
+      <div class="main-container-glass-more-info-content">
+        <div class="container-social-media">
+        <div class="container-whats">
+          <a href="https://api.whatsapp.com/send?phone=5532991361591" target="_blank">
+          <?xml version="1.0" encoding="UTF-8"?>
+          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
+            <path d="M35 17.5C35 27.1651 27.1651 35 17.5 35C7.83543 35 0 27.1651 0 17.5C0 7.8349 7.83543 0 17.5 0C27.1651 0 35 7.8349 35 17.5ZM22.7047 19.4635C22.4271 19.3247 21.0636 18.6543 20.8092 18.5614C20.5548 18.469 20.37 18.4226 20.1847 18.7002C20 18.9779 19.4686 19.6029 19.3064 19.7876C19.1449 19.9725 18.9834 19.9962 18.7058 19.8568C18.4281 19.7186 17.5349 19.4255 16.475 18.4807C15.6504 17.7457 15.0939 16.8374 14.9319 16.5597C14.7704 16.282 14.9148 16.1322 15.0537 15.9939C15.1786 15.8695 15.3313 15.6697 15.4695 15.5083C15.6084 15.3462 15.6549 15.2306 15.7467 15.0454C15.8396 14.8606 15.7932 14.6986 15.724 14.5598C15.6548 14.4209 15.1001 13.0559 14.8688 12.5005C14.6437 11.9601 14.4152 12.0332 14.2443 12.0248C14.0829 12.0165 13.8982 12.0148 13.7128 12.0148C13.5281 12.0148 13.2278 12.0844 12.9733 12.3616C12.719 12.6399 12.0027 13.3103 12.0027 14.6752C12.0027 16.0399 12.9965 17.3584 13.1354 17.5437C13.2737 17.7291 15.0911 20.5301 17.8733 21.7309C18.5355 22.0168 19.052 22.1872 19.4552 22.3149C20.1195 22.5262 20.7241 22.4963 21.202 22.425C21.7352 22.3453 22.8431 21.754 23.0743 21.1064C23.306 20.4586 23.306 19.9033 23.2364 19.7876C23.1666 19.6721 22.982 19.6029 22.7048 19.4636L22.7047 19.4635ZM17.6432 26.3719H17.6393C15.9839 26.3713 14.3599 25.9272 12.9433 25.0865L12.606 24.8863L9.11418 25.8023L10.0461 22.3983L9.8266 22.0493C8.90344 20.5808 8.41499 18.8833 8.41615 17.1405C8.41828 12.0544 12.5573 7.91669 17.647 7.91669C20.1112 7.91776 22.4282 8.87857 24.1705 10.622C25.9123 12.366 26.8709 14.6829 26.8703 17.1481C26.868 22.2346 22.7291 26.3719 17.6433 26.3719H17.6432ZM25.4963 9.29712C23.4005 7.19921 20.6128 6.04373 17.6432 6.04214C11.5241 6.04214 6.54381 11.0206 6.54159 17.1397C6.54097 19.0956 7.05215 21.0049 8.02335 22.6881L6.4487 28.4389L12.3333 26.8957C13.955 27.7796 15.7808 28.2459 17.6388 28.2464H17.6432C23.7617 28.2464 28.742 23.2679 28.7448 17.1488C28.746 14.1835 27.5921 11.3947 25.4963 9.2973V9.29712Z" fill="white"></path>
+          </svg>
+            <span>Tel.: (32) 991361591</span>
+          </a>
+        </div>
+        <div class="container-mail">
+          <a href="mailto:contato@rgsconsultoria.net.br" target="_blank">
+            <img src="<?= image('icons/icon_email.svg') ?>">
+            <span>contato@rgsconsultoria.net.br</span>
+          </a>
+        </div>
+        </div>
+      </div>
+    </div>
+
+
   </section>
 
   <?php partial('contact-form') ?>
